c538460eaaf6bcfcc66507d831c5acd1601bb419
nandakumar-kaliappan <133559109+nandakumar-kaliappan@users.noreply.github.com>
2023-05-14 14:53:46 +0530

    Update hello_world.py with enhancement (#1)
    Enhancement replacement

--- hello_world/src/hello_world.py
+++ hello_world/src/hello_world.py
@@ -1,3 +1,3 @@
 print("Hello, world!")
-print("This is my first GitHub respository.")
+print("This is my first GitHub respository with intense learning.")
 print("This repository will help me learn how to use GitHub!")
